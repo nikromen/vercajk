2918e2ab2b635847d7ce7d6ff68bff2df95173e2
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2024-04-21 20:52:35 +0200

    wip

--- ansible/roles/dotfiles/files/dotfiles/.config/hypr/scripts/workspaces.sh
+++ ansible/roles/dotfiles/files/dotfiles/.config/hypr/scripts/workspaces.sh
@@ -20,44 +20,69 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-if [ -z "$2" ]; then
-    echo "Error: Missing workspace number."
-    exit 1
-fi
 
-WORKSPACE_NUMBER=$2
-ACTIVE_MONITOR_NAME=$(hyprctl -j monitors | jq -r ".[] | select(.focused == true) | .name")
-EXISTING_WORKSPACE_ID=$(hyprctl -j workspaces | jq -r ".[] | select(.monitor == \"$ACTIVE_MONITOR_NAME\" and .name == \"$WORKSPACE_NUMBER\") | .id")
+MONITOR_LETTERS=("a" "b" "c" "d" "e" "f")
 
+
+refresh() {
+    local workspaces=$(hyprctl -j workspaces)
+    local monitors=$(hyprctl -j monitors | jq -r '.[].name')
+    local monitor_count=0
+    for monitor in $monitors; do
+        local monitor_workspaces=$(echo $workspaces | jq -r ".[] | select(.monitor == \"$monitor\") | .id" | sort)
+        local i=1
+        for workspace_id in $monitor_workspaces; do
+            hyprctl dispatch renameworkspace $workspace_id $i${MONITOR_LETTERS[$monitor_count]}
+            i=$((i + 1))
+        done
+        monitor_count=$((monitor_count + 1))
+    done
+}
+
+_prep() {
+    WORKSPACE_NUMBER=$2
+    ACTIVE_MONITOR_NAME=$(hyprctl -j monitors | jq -r ".[] | select(.focused == true) | .name")
+    ACTIVE_WORKSPACE=$(hyprctl -j activeworkspace | jq -r '.name')
+    MONITOR_LETTER=$(echo $ACTIVE_WORKSPACE | sed 's/.*\([a-z]\)$/\1/')
+    NEW_WORKSPACE_NAME=$WORKSPACE_NUMBER$MONITOR_LETTER
+    WORKSPACE_ID=$(hyprctl -j workspaces | jq -r ".[] | select(.monitor == \"$ACTIVE_MONITOR_NAME\" and .name == \"$NEW_WORKSPACE_NAME\") | .id")
+}
 
 new() {
-    if [ -n "$EXISTING_WORKSPACE_ID" ]; then
-        hyprctl dispatch workspace $EXISTING_WORKSPACE_ID
+    _prep $@
+
+    if [ -n "$WORKSPACE_ID" ]; then
+        hyprctl dispatch workspace $WORKSPACE_ID
+        echo "ahoj"
         exit 0
     fi
 
     hyprctl dispatch workspace empty
-    local id=$(hyprctl -j activeworkspace | jq -r '.id')
-    hyprctl dispatch renameworkspace $id $WORKSPACE_NUMBER
-    echo $id
+    local new_workspace_id=$(hyprctl -j activeworkspace | jq -r '.id')
+    hyprctl dispatch renameworkspace $new_workspace_id $NEW_WORKSPACE_NAME
+    echo $new_workspace_id
 }
 
 move() {
-    local id=
-    local active_window=
-    local active_workspace=$(hyprctl -j activeworkspace | jq -r ".id")
-    if [ -z "$EXISTING_WORKSPACE_ID" ]; then
-        active_window=$(hyprctl -j activewindow | jq -r ".address")
-        id=$(new)
+    _prep $@
+    if [ -z "$WORKSPACE_ID" ]; then
+        local last_workspace_id=$(hyprctl -j workspaces | jq -r ".[] | select(.monitor == \"$ACTIVE_MONITOR_NAME\") | .id" | sort | tail -n 1)
+        id=$((last_workspace_id + 1))
     else
-        id=$EXISTING_WORKSPACE_ID
+        id=$WORKSPACE_ID
     fi
 
-    if [ -n "$active_window" ]; then
-        hyprctl dispatch movetoworkspace $active_workspace,$active_window
-    else
-        hyprctl dispatch workspace $active_workspace
-        hyprctl dispatch movetoworkspace $id
+    hyprctl dispatch movetoworkspace $id
+    if [ -z "$WORKSPACE_ID" ]; then
+        $HOME/.config/hypr/scripts/workspaces.sh -r
+    fi
+}
+
+
+check() {
+    if [ -z "$2" ]; then
+        echo "Error: Missing workspace number."
+        exit 1
     fi
 }
 
@@ -68,10 +93,12 @@ case $1 in
         exit 0
         ;;
     -n|--new)
-        new
+        check $@
+        new $@
         ;;
     -m|--move)
-        move
+        check $@
+        move $@
         ;;
     -r|--refresh)
         refresh
