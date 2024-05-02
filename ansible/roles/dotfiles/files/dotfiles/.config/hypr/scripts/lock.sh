c5a53fe8a5a4f1b3e958f8bf98f9a43b4e371441
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2024-05-03 00:28:20 +0200

    wip

--- ansible/roles/dotfiles/files/dotfiles/.config/hypr/scripts/lock.sh
+++ ansible/roles/dotfiles/files/dotfiles/.config/hypr/scripts/lock.sh
@@ -2,6 +2,6 @@
 
 # deal with stupid workspaces
 
-pidof hyprlock || hyprlock  # so hyprlock is called only once
+hyprlock  # so hyprlock is called only once
 $HOME/.config/hypr/scripts/workspaces.sh --refresh
-killall waybar && waybar & disown
+killall waybar && waybar
